2921fbfeeb33d11ff456ef50493f5e1e5149265b
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-01-18 19:04:43 +0100

    ADD cita component

--- App.js
+++ App.js
@@ -1,26 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+import Cita from './componentes/Cita';
 
 const App = () => {
+  const [citas, setCitas] = useState([
+    {id: '1', paciente: 'Hook', propietario: 'Alejandro', sintomas: 'no come'},
+    {id: '2', paciente: 'Redux', propietario: 'Pedro', sintomas: 'no duerme'},
+    {id: '3', paciente: 'Native', propietario: 'Juan', sintomas: 'no canta'},
+  ]);
+
   return (
-    <>
-      <Text style={styles.encabezado}>Hola Mundo</Text>
-    </>
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Administrador de Citas</Text>
+
+      <FlatList
+        data={citas}
+        keyExtractor={(cita) => cita.id}
+        renderItem={({item}) => <Cita cita={item} />}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  encabezado: {
+  container: {
+    backgroundColor: '#aa076b',
+    flex: 1,
+  },
+  titulo: {
     textAlign: 'center',
-    marginTop: 120,
+    color: '#fff',
+    marginTop: 50,
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
 
